6c5256011e245f8eab55ec6c8fdee9effe459e8d
federico.tomadin <federico.tomadin@hotmail.com>
2020-01-16 23:09:11 -0300

    publicando en heroku

--- bd/AccesoDatos.php
+++ bd/AccesoDatos.php
@@ -6,18 +6,20 @@ class AccesoDatos
  
     private function __construct()
     {
-        $username = 'u663828753_admin';
+        $username = 'b79b71c21d6cb4';
         $servername= 'mysql.hostinger.com.ar';
         $database= 'u663828753_resta';
-        $password= 'o5bB2zUQ30oB';
+        $password= '225fbb00';
         try {     
        
-           $this->_objetoPDO = new PDO('mysql:host=localhost;dbname=u663828753_resta; charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            mysql://b79b71c21d6cb4:225fbb00@us-cdbr-iron-east-05.cleardb.net/heroku_7869f6aa8a7abd0?reconnect=true
 
-         /*  $this->_objetoPDO = new PDO("mysql:host=localhost;dbname=u663828753_resta", $username ,$password);
+          /* $this->_objetoPDO = new PDO('mysql:host=localhost;dbname=u663828753_resta; charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));*/
+
+           $this->_objetoPDO = new PDO("mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_7869f6aa8a7abd0", $username ,$password);
 
             $this->_objetoPDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $this->_objetoPDO->exec("SET CHARACTER SET utf8");*/
+            $this->_objetoPDO->exec("SET CHARACTER SET utf8");
  
         } catch (PDOException $e) {
  
